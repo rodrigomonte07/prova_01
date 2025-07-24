25c398c51d921103cde0c1e32d114e449922625f
RODRIGO MONTE COSTA <rodrigomontecosta@gmail.com>
2025-07-23 23:34:52 -0300

    Add files via upload

---
+++ logica710.py
@@ -0,0 +1,9 @@
+print("Por favor, forneça os dados a seguir para que possamos calcular a área do seu retângulo.")
+
+
+altura = float(input("Digite a altura: "))
+base = float(input( "Digite a base :"))
+
+area = base * altura
+
+print(f"A área do retângulo com a base {base} e a altura {altura} é igual a {area}")
